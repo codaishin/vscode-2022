ed43d1f5b3c7b9ecd7109d669921959d47e618c4
codaishin <codaishin@gmail.com>
2022-09-22 07:26:39 +0000

    numbers

---
+++ csharp/numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int start, end, steps;
+
+do {
+	Console.Write("from: ");
+} while (!int.TryParse(Console.ReadLine(), out start));
+do {
+	Console.Write("to: ");
+} while (!int.TryParse(Console.ReadLine(), out end));
+do {
+	Console.Write("steps: ");
+} while (!int.TryParse(Console.ReadLine(), out steps));
+
+for (int number = start; number <= end; number += steps) {
+	Console.WriteLine(number);
+}
