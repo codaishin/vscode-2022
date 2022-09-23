94b8596a2c40ef4a6bd926448129acb37d0ce877
codaishin <codaishin@gmail.com>
2022-09-23 09:20:30 +0200

    add fibbonacci csharp

---
+++ fibonacci_csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int fst = 0;
+int snd = 1;
+
+int from;
+int to;
+
+Console.Write("from: ");
+from = Convert.ToInt32(Console.ReadLine());
+Console.Write("to: ");
+to = Convert.ToInt32(Console.ReadLine());
+
+while (snd <= to) {
+	if (snd >= from) {
+		Console.WriteLine(snd);
+	}
+	int tmp = fst + snd;
+	fst = snd;
+	snd = tmp;
+}
